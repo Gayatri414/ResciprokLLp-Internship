9763d094a730203f1814195d86086b29382141c9
Gayatri414 <gayatrigade7949@gmail.com>
2026-03-04 00:33:23 +0530

    Configure production setup for frontend + backend together

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 import app from './app.js';
 import connectDB from './config/db.js';
 import { env } from './config/env.js';
+import path from "path";
 
 connectDB();
+
+const __dirname = path.resolve();
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const PORT = env.PORT || 5000;
 const server = app.listen(PORT, () => {
